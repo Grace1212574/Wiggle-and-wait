be635971d0209366dd5775bc0ed710bc0fb195ce
Grace1212574 <24146@burnside.school.nz>
2026-06-15 14:39:57 +1200

    leafs apples spawn less

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,7 @@
 
 extends Node2D
+@export var max_apples: int = 15
+@export var max_leaf: int = 7
 
 
 var leaf_prefab = preload("res://leaf.tscn")
@@ -12,19 +14,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+ 
 
 func _on_appletimer_timeout() -> void:
-	var apple = apple_prefab.instantiate()
-	add_child(apple)
-	var random_x = randi_range(-665,804)
-	var random_y = randi_range(-422,485)
-	apple.global_position = Vector2(random_x,random_y)
-
+	var current_apple = get_tree().get_nodes_in_group("apples").size()
+	if current_apple < max_apples:
+		var apple = apple_prefab.instantiate()
+		add_child(apple)
+		var random_x = randi_range(-665,804)
+		var random_y = randi_range(-422,485)
+		apple.global_position = Vector2(random_x,random_y)
 
 func _on_leaftimer_timeout() -> void:
-	var leaf = leaf_prefab.instantiate()
-	get_tree().current_scene.add_child(leaf)
-	var random_x = randi_range(-665,804)
-	var random_y = randi_range(-422,485)
-	leaf.global_position = Vector2(random_x,random_y)
+	var current_leaf = get_tree().get_nodes_in_group("leaf").size()
+	if current_leaf < max_leaf:
+		var leaf = leaf_prefab.instantiate()
+		get_tree().current_scene.add_child(leaf)
+		var random_x = randi_range(-665,804)
+		var random_y = randi_range(-422,485)
+		leaf.global_position = Vector2(random_x,random_y)
+	
