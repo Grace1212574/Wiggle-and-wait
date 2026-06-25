50a5ed6af45847e469d17fdea7bd74effea90821
Grace1212574 <24146@burnside.school.nz>
2026-06-25 14:41:31 +1200

    bird

--- oblects/bird.gd
+++ oblects/bird.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 const FLY_SPEED = 600.0
 
+func _ready() -> void:
+	get_tree().create_timer(4.0).timeout.connect(queue_free)
+	
 func _physics_process(delta: float) -> void:
 	position.x += FLY_SPEED * delta
 	
-	if position.x > 1200: 
-		queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
--- scripts/level.gd
+++ scripts/level.gd
@@ -4,7 +4,6 @@ extends Node2D
 @export var max_apples: int = 13
 @export var max_leaf: int = 7
 
-
 @onready var warning_label = $CanvasLayer2/warninglabel
 @onready var player = $player
 @onready var bird_timer = $birdtimer
@@ -14,12 +13,18 @@ var bird_prefab = preload("res://bird.tscn")
 var leaf_prefab = preload("res://leaf.tscn")
 var apple_prefab = preload("res://apple.tscn")
 
-
 func _ready() -> void:
+	
+	bird_timer.one_shot = true
+	bird_timer.autostart = false
+	lable_timer.one_shot = true
+	lable_timer.autostart = false
+	
+	warning_label.hide()
+	
+	await get_tree().create_timer(2.0).timeout
 	start_random_bird_timer()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 	
@@ -28,50 +33,44 @@ func _on_appletimer_timeout() -> void:
 	if current_apple < max_apples:
 		var apple = apple_prefab.instantiate()
 		add_child(apple)
-		var random_x = randi_range(-665,804)
-		var random_y = randi_range(-422,485)
-		apple.global_position = Vector2(random_x,random_y)
+		var random_x = randi_range(-665, 804)
+		var random_y = randi_range(-422, 485)
+		apple.global_position = Vector2(random_x, random_y)
 
 func _on_leaftimer_timeout() -> void:
 	var current_leaf = get_tree().get_nodes_in_group("leaf").size()
 	if current_leaf < max_leaf:
 		var leaf = leaf_prefab.instantiate()
 		get_tree().current_scene.add_child(leaf)
-		var random_x = randi_range(-665,804)
-		var random_y = randi_range(-422,485)
-		leaf.global_position = Vector2(random_x,random_y)
+		var random_x = randi_range(-665, 804)
+		var random_y = randi_range(-422, 485)
+		leaf.global_position = Vector2(random_x, random_y)
 		
 func start_random_bird_timer() -> void:
 	bird_timer.wait_time = randf_range(10.0, 20.0)
 	bird_timer.start()
-	
-
 
 func _on_birdtimer_timeout() -> void:
+	if player == null: return
+	
 	if not player.visible: 
 		start_random_bird_timer()
-		
 	else:
-		print("player caught")
 		warning_label.show()
+		lable_timer.wait_time = 5.0 # Forcing a 5-second window to react
 		lable_timer.start()
 
-
 func _on_label_timer_timeout() -> void:
-	print("TIMER FINISHED! Spawning bird now...")
 	warning_label.hide()
-	$countdown_timer.stop()
 	
 	if player.visible:
 		var bird = bird_prefab.instantiate()
 		add_child(bird)
-		bird.global_position = Vector2(player.global_position.x - 700, player.global_position.y)
-		var anim = bird.get_node("AnimatedSprite2D")
+		
+		bird.global_position = Vector2(player.global_position.x - 500, player.global_position.y)
+		
+		var anim = bird.get_node_or_null("AnimatedSprite2D")
 		if anim:
 			anim.play("default")
-		print("The bird caught you!")
-		start_random_bird_timer()
-		
 	else:
 		start_random_bird_timer()
-	
