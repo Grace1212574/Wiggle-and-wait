912525bfe6ea97bde5def6bca08c5e1fbac2b64d
Grace1212574 <24146@burnside.school.nz>
2026-07-24 12:34:07 +1200

    lots done

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,13 +2,12 @@
 extends Node2D
 
 @export var max_apples: int = 13
-@export var max_leaf: int = 7
 @export var bird_prefab: PackedScene = preload("res://bird.tscn")
 
 @onready var warning_label = $CanvasLayer2/warninglabel
 @onready var player = $player
 @onready var bird_timer = $birdtimer
-@onready var lable_timer = $labelTimer
+@onready var label_timer = $labelTimer
 
 
 var leaf_prefab = preload("res://leaf.tscn")
@@ -18,11 +17,14 @@ func _ready() -> void:
 	
 	bird_timer.one_shot = true
 	bird_timer.autostart = false
-	lable_timer.one_shot = true
-	lable_timer.autostart = false
+	label_timer.one_shot = true
+	label_timer.autostart = false
 	
 	warning_label.hide()
 	
+	for i in range(max_apples):
+		_on_appletimer_timeout()
+		
 	await get_tree().create_timer(2.0).timeout
 	start_random_bird_timer()
 
@@ -33,17 +35,21 @@ func _on_appletimer_timeout() -> void:
 	var current_apple = get_tree().get_nodes_in_group("apples").size()
 	if current_apple < max_apples:
 		var apple = apple_prefab.instantiate()
-		add_child(apple)
-		var random_x = randi_range(-665, 804)
-		var random_y = randi_range(-422, 485)
+		apple.add_to_group("apples")
+		
+		var random_x = randf_range(-3014.0,2410.0 )
+		var random_y = randf_range(-1471.0,1578.0)
 		apple.global_position = Vector2(random_x, random_y)
+		
+		for leaf in get_tree().get_nodes_in_group("hide"):
+			if apple.global_position.distance_to(leaf.global_position) < 80.0:
+				return
+			
+		add_child(apple)
 
 func _on_leaftimer_timeout() -> void:
-	var current_leaf = get_tree().get_nodes_in_group("leaf").size()
-	if current_leaf < max_leaf:
-		var leaf = leaf_prefab.instantiate()
-		get_tree().current_scene.add_child(leaf)
-		
+	pass
+ 
 		
 func start_random_bird_timer() -> void:
 	bird_timer.wait_time = randf_range(10.0, 20.0)
@@ -56,8 +62,8 @@ func _on_birdtimer_timeout() -> void:
 		start_random_bird_timer()
 	else:
 		warning_label.show()
-		lable_timer.wait_time = 5.0 # Forcing a 5-second window to react
-		lable_timer.start()
+		label_timer.wait_time = 5.0 # Forcing a 5-second window to react
+		label_timer.start()
 
 func _on_label_timer_timeout() -> void:
 	warning_label.hide()
@@ -73,3 +79,5 @@ func _on_label_timer_timeout() -> void:
 			anim.play("default")
 	else:
 		start_random_bird_timer()
+		
+		
