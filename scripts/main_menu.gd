63daf98be77a0c2314c334e9290c4ed8b17e5428
Grace1212574 <24146@burnside.school.nz>
2026-08-12 09:04:31 +1200

    sound

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,10 +4,7 @@ extends CanvasLayer
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
-	
 
-func _on_highscore_pressed() -> void:
-	get_tree().change_scene_to_file("res://highscore.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
